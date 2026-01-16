8a82db46bada08af67df226e2aabe4d5a181ca67
Bittu <raosahab7645@gmail.com>
2026-01-16 23:55:02 +0530

    Education Added

--- portfolio.js
+++ portfolio.js
@@ -34,4 +34,16 @@ hamburger.addEventListener("click", () => {
 
 closeBtn.addEventListener("click", () => {
     menu.style.display = "none";
-})
+});
+
+// Education result 
+let bca_mark = document.querySelector(".bca_mark");
+let inter_mark = document.querySelector(".inter_mark");
+let matric_mark = document.querySelector(".matric_mark");
+let mark1 = (2393 / 3200)*100;
+let mark2 = (313 / 500)*100;
+let mark3 = (307 / 500)*100;
+console.log(mark1);
+bca_mark.innerHTML = Math.floor(mark1 * 100)/100 + "%";
+inter_mark.innerText = Math.round(mark2 * 100)/100 + "%";
+matric_mark.textContent = Math.ceil(mark3 * 100)/100 + "%";
